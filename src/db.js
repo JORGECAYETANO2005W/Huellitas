3050412bad5939f123f9bf2db90e0e64bede762b
JORGECAYETANO2005W <jorgecayetano227@gmail.com>
2025-03-21 00:50:10 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,13 +1,17 @@
 
 import mongoose from "mongoose"; // Importar mongoose para trabajar con MongoDB
+import mongoose from "mongoose"; 
 
 // Función para conectar a la base de datos MongoDB
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/Huellitas"); // Conectar a la base de datos 
-        console.log("MongoDB conectado"); // Mensaje de éxito al conectar
+        await mongoose.connect( 
+        //"mongodb://127.0.0.1:27017/AgriStore"
+        "mongodb+srv://JMCG2005:JMCG2005@huellitas.ykwou.mongodb.net/Huellitas?retryWrites=true&w=majority&appName=JMCG2005" // URI de conexión a MongoDB Atlas
+        );
+        console.log("MongoDB conectado");
     } catch (error) {
-        console.error("Error al conectar MongoDB:", error); // Mostrar el error si la conexión falla
+        console.error("Error al conectar MongoDB:", error);
     }
 };
 
